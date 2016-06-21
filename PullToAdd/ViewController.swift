aa2a57a5a3d28235b1f1e53070d55c6442cc6417
Harry Ng <harryworld@gmail.com>
2016-06-21 21:39:59 +0800

    Initial Commit

---
+++ PullToAdd/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PullToAdd
+//
+//  Created by Harry Ng on 21/6/2016.
+//  Copyright © 2016 STAY REAL LIMITED. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
