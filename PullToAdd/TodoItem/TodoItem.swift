b79dd7b389b8e60d0766e925f65b4844887dbfcc
Harry Ng <harryworld@gmail.com>
2016-06-21 22:26:00 +0800

    Add CollectionView

---
+++ PullToAdd/TodoItem/TodoItem.swift
@@ -0,0 +1,18 @@
+//
+//  TodoItem.swift
+//  PullToAdd
+//
+//  Created by Harry Ng on 21/6/2016.
+//  Copyright © 2016 STAY REAL LIMITED. All rights reserved.
+//
+
+import Cocoa
+
+class TodoItem: NSCollectionViewItem {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
--- PullToAdd/ViewController.swift
+++ PullToAdd/ViewController.swift
@@ -10,18 +10,36 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var collectionView: NSCollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
+}
 
+extension ViewController: NSCollectionViewDataSource {
+    
+    func collectionView(collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: NSCollectionView, itemForRepresentedObjectAtIndexPath indexPath: NSIndexPath) -> NSCollectionViewItem {
+        let item = collectionView.makeItemWithIdentifier("TodoItem", forIndexPath: indexPath)
+        
+        item.textField?.stringValue = "TodoList \(indexPath.item)"
+        
+        return item
+    }
 
 }
 
+extension ViewController: NSCollectionViewDelegateFlowLayout {
+    
+    func collectionView(collectionView: NSCollectionView, layout collectionViewLayout: NSCollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> NSSize {
+        return NSSize(width: collectionView.bounds.width, height: 50)
+    }
+    
+}
